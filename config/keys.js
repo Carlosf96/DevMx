e98b54152dcdb7694741b0b267e6a70d5635965b
Carlosf96 <carlos.f.roque96@gmail.com>
2019-07-19 22:32:45 -0500

    connected express server to mongodb cluster

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://xex:A987654zro@cluster0-cqkrf.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');//pull in express
+const mongoose = require('mongoose');
 
 
 const app = express();//init express
+//db config
+const db = require('./config/keys').mongoURI;
+
+//Connect to mongoDB
+
+mongoose.connect(db, {
+  useNewUrlParser: true
+})
+  .then(() => console.log('mongoDB connected successfully'))
+  .catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('hello'));//basic route
 
