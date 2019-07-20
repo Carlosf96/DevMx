db815d3b32f1d66acbacc6045636c5eb59f68206
Carlosf96 <carlos.f.roque96@gmail.com>
2019-07-20 16:27:18 -0500

    Create some routes to use with express

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');//pull in express
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();//init express
-//db config
+
+//DB configuration 
 const db = require('./config/keys').mongoURI;
 
 //Connect to mongoDB
@@ -15,6 +19,11 @@ mongoose.connect(db, {
   .catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('hello'));//basic route
+//expose our routes to our express server
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 
 const port = 5000 || process.env.PORT;//port local or on heroku
 
