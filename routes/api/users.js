9719fad31a9e0d665a0956e51e2498d73bea0c61
Carlosf96 <carlos.f.roque96@gmail.com>
2019-07-29 07:20:49 -0500

    will continue working on authentication/setting and getting JWT tokens

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,72 +10,86 @@ const User = require('../../models/User');
 // @route  GET api/users/test 
 // @desc   Tests users route
 // @access Public
-router.get('/test', (req, res) => res.json({msg: 'User works'}));
+router.get('/test', (req, res) => res.json({
+  msg: 'User works'
+}));
 
 // @route  GET api/users/register
 // @desc   Register user
 // @access Public
-router.post('/register', (req, res)=> {
-  User.findOne({ email: req.body.email })
-  .then(user => {
-    if(user){
-      return res.status(400).json({email: 'Email Already Exists'});
-    } else {
-      const avatar = gravatar.url(req.body.email, {
-        s: '200',//size
-        r: 'pg', //rating
-        d: 'mm'//default
-        
-      })
+router.post('/register', (req, res) => {
+  User.findOne({
+      email: req.body.email
+    })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({
+          email: 'Email Already Exists'
+        });
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', //size
+          r: 'pg', //rating
+          d: 'mm' //default
 
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        avatar,
-        password: req.body.password
-      });
-
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser.save()
-          .then(user => res.json(user))
-          .catch(err=> console.log(err))
         })
-      });
 
-    }
-  })
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err))
+          })
+        });
+
+      }
+    })
 });
 
 // @route  GET api/users/login
 // @desc   Login user and return JWT Token
 // @access Public
 
-router.post('/login', (req, res)=>{
+router.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
   // Find User by Email
 
-  User.findOne({email})
-  .then(user => {
-    // Check for user
-    if(!user){
-      return res.status(404).json({email: 'User email not found'});
-    }
-    // Check password
-    bcrypt.compare(password, user.password)
-    .then(isMatch => {
-      if(isMatch){
-        res.json({msg: 'Sucess'})
-      } else {
-        res.status(400).json({password: 'Password Incorrect'})
-      }
+  User.findOne({
+      email
     })
+    .then(user => {
+      // Check for user
+      if (!user) {
+        return res.status(404).json({
+          email: 'User email not found'
+        });
+      }
+      // Check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({
+              msg: 'Sucess'
+            })
+          } else {
+            res.status(400).json({
+              password: 'Password Incorrect'
+            })
+          }
+        })
 
-  });
+    });
 
 })
 
