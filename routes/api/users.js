518b422fac847fb66270367dfd887763de4370be
Carlosf96 <carlos.f.roque96@gmail.com>
2019-09-04 06:29:13 -0500

    Almost done with user input validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
-
+const validateLoginInput = require('../../validation/login');
 // Load User model
 const User = require('../../models/User');
 
@@ -66,15 +66,20 @@ router.post('/register', (req, res) => {
 // @access Public
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const { email, password } = req.body;
 
   // Find User by Email
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({
-        email: 'User email not found'
-      });
+      errors.email = 'User not found'
+      return res.status(404).json(errors);
     }
     // Check password
     bcrypt.compare(password, user.password).then(isMatch => {
@@ -99,9 +104,8 @@ router.post('/login', (req, res) => {
           }
         );
       } else {
-        res.status(400).json({
-          password: 'Password Incorrect'
-        });
+        errors.password = 'Password incorrect'
+        res.status(400).json(errors);
       }
     });
   });
--- validation/register.js
+++ validation/register.js
@@ -4,9 +4,33 @@ const isEmpty = require( './isEmpty');
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
+
   if (!Validator.isLength(data.name, { min: 4, max: 16})) {
     errors.name = 'Name must between 4 and 16 characters';
   }
+  if (!Validator.isEmail(data.email)) { 
+    errors.email = 'Email is invalid';
+  }
+  if (Validator.isEmpty(data.email)) { 
+    errors.email = 'Email field required';
+  }
+  if (Validator.isEmpty(data.password)) { 
+    errors.password = 'Password field required';
+  }
+  if (!Validator.isLength(data.password, { min: 4, max: 16})) {
+    errors.password = 'Password must between 4 and 16 characters';
+  }
+  if (Validator.isEmpty(data.password2)) { 
+    errors.password2 = 'Please Confirm Password';
+  }
+  if (!Validator.equals(data.password. data.password2)) {
+    errors.password2 = 'Passwords do not match'
+  }
+
   return {
     errors,
     isValid: isEmpty(errors)
